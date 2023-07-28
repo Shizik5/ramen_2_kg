fbc8b076dd7d2f55c033516d6549a9ba1762a742
Shizik5 <amyrsabekova@mail.ru>
2023-07-28 17:42:45 +0600

    f

--- comment/tasks.py
+++ comment/tasks.py
@@ -8,7 +8,7 @@ def send_new_comment_email(comment_id):
 
     subject = 'New Comment on Your Post'
     message = f'Hi {comment.post.owner},\n\nYou have a new comment on your post: "{comment.body}"\n\nBest regards,\nYour Website Team'
-    from_email = 'darinaibrag@gmail.com'
+    from_email = 'sadyr.top@gmail.com'
     recipient_list = [comment.post.owner.email]
 
     send_mail(subject, message, from_email, recipient_list)
